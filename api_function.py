6c25c5b110bb2cf34ecd7227726f3d5fcbbdf8f9
Fadil <m.muhammadfadil12@gmail.com>
2023-05-28 11:43:18 +0700

    add audio extraction from video

--- api_function.py
+++ api_function.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from keras.models import load_model
+from moviepy.editor import VideoFileClip
 
 model = load_model('model.h5')
 
@@ -91,3 +92,14 @@ def predict_video(video):
     frames_folder = extract_frames(video)
     total_predictions = predict_all_frames(frames_folder)
     return total_predictions
+
+def extract_audio_from_video(video_path, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    video = VideoFileClip(video_path)
+    audio = video.audio
+    filename = os.path.splitext(os.path.basename(video_path))[0]
+    output_path = os.path.join(output_dir, f"{filename}.wav")
+    audio.write_audiofile(output_path, codec='pcm_s16le')
+    audio.close()
+    video.close()
+    return output_path
